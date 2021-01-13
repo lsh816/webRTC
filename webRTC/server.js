91446504f7141230aeb559ac89230ee24d307751
leesohyeon <isohyeon0816@gmail.com>
2021-01-13 16:06:53 +0900

    create webRTC
    openSSL을 통한 webRTC 구현 완료

---
+++ webRTC/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const { v4: uuidV4 } = require('uuid');
+
+const fs = require('fs');
+const https = require('https');
+const server = https.createServer(
+  {
+    // https 프로토콜 사용을 위한 ssl 발급
+    key: fs.readFileSync('./private.pem'),
+    cert: fs.readFileSync('./public.pem'),
+    requestCert: false,
+    rejectUnauthorized: false,
+  },
+  app
+);
+
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    });
+  });
+});
+
+server.listen(3000);
